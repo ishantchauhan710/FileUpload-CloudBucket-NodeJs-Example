783256abc978be40b7f115b0bc553a04d8719da8
ishant <ishantchauhan710@gmail.com>
2022-06-12 20:51:40 +0530

    File upload to bucket functionality added

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const fs = require("fs"); // File system module for file handling
 const path = require("path"); // Path module for path handling
 const crypto = require("crypto"); // Crypto module for random data generation
 const multer = require("multer"); // Multer for file upload handling
+const dotenv = require("dotenv"); // For handling environment variables
+const aws = require("aws-sdk"); // For handling cloud hosting bucket
+
+dotenv.config();
 
 const app = express(); // Express App Object
 
@@ -44,11 +48,15 @@ app.post(
   multer({
     storage: multerStorage, // Multer storage engine
   }).single("upload"), // Single file upload
-  (req, res) => {
-    console.log(req.file); // In callback function, we display the uploaded data
-    console.log(req.body);
+  async (req, res) => {
+    //console.log(req.file); // In callback function, we display the uploaded data
+    //console.log(req.body); await uploadFileToCloud("req.file.filename","/uploads/" + req.file.filename); // Finally upload the file to hosting's bucket
+    await uploadFileToCloud(
+      req.file.filename,
+      path.join(__dirname, `/uploads/${req.file.filename}`)
+    ); // Finally upload the file to hosting's bucket
     res.redirect("/uploads/" + req.file.filename); // And redirect user to the newly uploaded file's location
-    console.log(req.file.filename);
+    //console.log(req.file.filename);
     return res.status(200).end();
   }
 );
@@ -56,11 +64,62 @@ app.post(
 // We can use this endpoint to get the uploaded file [Only images can be rendered on webpage]
 app.get("/uploads/:upload", (req, res) => {
   const file = req.params.upload;
-  console.log(req.params.upload);
+  //console.log(req.params.upload);
   const img = fs.readFileSync(__dirname + "/uploads/" + file);
   res.writeHead(200, { "Content-Type": "image/png" });
   res.end(img, "binary");
 });
+
+// Function to upload file to hosting bucket
+const uploadFileToCloud = async (fileName, file) => {
+  if (!file) return console.log("Please provide a file path");
+
+  // Initialize all credentials
+  const accessKey = process.env.STORAGE_ACCESS_KEY;
+  const secretKey = process.env.STORAGE_SECRET_KEY;
+  const storageBucket = process.env.STORAGE_BUCKET;
+  const storageRegion = process.env.STORAGE_REGION;
+
+  // Replace this with your storage bucket url
+  const storageUrl = `https://${storageRegion}.linodeobjects.com`;
+
+  // Create a client that can access the storage bucket
+  const client = new aws.S3({
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey,
+    endpoint: new aws.Endpoint(storageUrl),
+  });
+
+  // Read the file stored on your machine
+  fs.readFile(file, {}, async (error, data) => {
+    if (error) {
+      console.log("File path: ", file);
+      return console.log("Error reading file :" + error);
+    }
+
+    console.log("File Name:", fileName);
+
+    // Move that file to the storage bucket
+    try {
+      await client
+        .putObject({
+          Bucket: storageBucket,
+          Key: fileName,
+          Body: data,
+        })
+        .promise();
+
+      // Then delete the file from your machine
+      fs.unlink(file, () => {
+        console.log("File deleted from machine");
+      });
+
+      console.log(`File uploaded successfully: ${file}`);
+    } catch (e) {
+      console.error("Error uploading file: ", e);
+    }
+  });
+};
 
 // Start our server
 app.listen(7000, () => {
